f3d8451f2ff48b1288a7815229fb0e9f53a14cc4
Honey Jose <84311863+pixelin-fepanaise@users.noreply.github.com>
2024-09-18 23:50:43 +0530

    added Docker setup

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/caller-analytics-graph/",
+  server: {
+    host: true,
+    port: 5173,
+    watch: {
+      usePolling: true,
+    },
+  },
 });
